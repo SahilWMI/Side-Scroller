e5b41bd6ae01dd2a97ad3fe7e5e2f1d53d54cca5
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-09-14 00:02:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ class Game {
         this.ctx.fillRect(100, 100, 50, 150);
     }
 }
+
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
+
+    const game = newGame(canvas,ctx);
+    game.render();
+});
